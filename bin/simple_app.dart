ffa366c207c55361b984b694e28262ae5033758c
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-08-30 13:44:42 -0700

    Learned a ton

--- bin/simple_app.dart
+++ bin/simple_app.dart
@@ -1,28 +1,5 @@
-void main() {
-  List<int> one = [1, 2, 4, 6, 7];
-  print(binarySearch(one, 2));
-  print(binarySearch(one, 1));
-  print(binarySearch(one, 4));
-  print(binarySearch(one, 33));
-  print(binarySearch(one, 6));
-}
+import "package:simple_app/using_process.dart";
+import "package:simple_app/using_timer.dart";
 
-int binarySearch(List<int> nums, int target) {
-  int idx = -1;
-  int start = 0;
-  int end = nums.length - 1;
-
-  while (start <= end) {
-    int mid = (start + end) ~/ 2;
-    int curr = nums[mid];
-    if (curr == target) {
-      idx = mid;
-      break;
-    } else if (curr > target) {
-      end = mid - 1;
-    } else if (curr < target) {
-      start = mid + 1;
-    }
-  }
-  return idx;
-}
+// make sure to put coding files in the lib directory!
+void main() {}
